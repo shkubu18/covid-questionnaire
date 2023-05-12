5c29e9c31333a08a3d4b7cb7c958d26eb2d8d5ff
data <data.shkubuliani@redberry.ge>
2023-05-12 22:30:13 +0400

    feat: add home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,12 @@
 <template>
-    <h1></h1>
+    <div class="h-screen flex items-center justify-center flex-col bg-home-background">
+        <icon-redberry class="w-logo-width"/>
+        <router-link class="mt-20 px-20 pb-20 hover:text-shadow" to="kitkhvari/piradi-informacia">
+            <h1 class="ml-3 text-center font-bold text-3xl">კითხვარის <br/> დაწყება</h1>
+        </router-link>
+    </div>
 </template>
+
+<script setup>
+import IconRedberry from "@/components/icons/IconRedberry.vue";
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,20 @@ export default {
         "./src/**/*.{vue,js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'home-background': '#EAEAEA',
+            },
+            width: {
+                'logo-width': '100px'
+            },
+            textShadow: {
+                'default': '1px 1px 0px white, 2px 1px 0px black'
+            }
+        },
     },
-    plugins: [],
+    plugins: [
+        require('tailwindcss-textshadow')
+    ],
 }
 
