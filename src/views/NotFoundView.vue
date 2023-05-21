7242333cea926fc7daa4c56543b30dd8926cf339
data <data.shkubuliani@redberry.ge>
2023-05-21 19:44:21 +0400

    feat: add notfound component * add notfound route

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,13 @@
+<template>
+  <the-main>
+    <div class="flex items-center flex-col">
+      <h1 class="text-5xl m-auto">სამწუხაროდ გვერდი ვერ მოიძებნა!</h1>
+      <router-link :to="{name: 'home'}" class="text-4xl mt-10 text-blue-600">
+        👉 <span class="underline">მთავარი გვერდი</span>
+      </router-link>
+    </div>
+  </the-main>
+</template>
+<script setup>
+import TheMain from "@/components/shared/TheMain.vue";
+</script>
